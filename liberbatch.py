73339add73074b3c1f69c687c8b1a958aa7401c0
Ferenc Cole Thierrin <159455998+CThierrin@users.noreply.github.com>
2024-05-31 08:56:47 -0400

    Fix multiple bugs

--- liberbatch.py
+++ liberbatch.py
@@ -1,14 +1,19 @@
-import os, liberupdate, meichecker
+import os, meichecker, liberupdatev5
 
 directory = os.fsencode("C:\\Users\\cole_\\Downloads\\liberUsualis - Copy\\liber")
 err=""
+
+i = 0 #for debugging
 for mefile in os.listdir(directory):
     filename = os.fsdecode(mefile)
     if filename.endswith("corr.mei"):
         liberupdate.main(filename)
         print(filename +" has been updated")
-        err= err+ (meichecker.main(filename[:-4]+"NEW.mei")+"\n")
+        err= err+ (meichecker.main(filename[:-4]+"NEW2.mei")+"\n")
 #        print(err)
         print(filename +" has been checked")
+     #   i+=1
 
+    #if i>=100:
+    #    break
 print(err)
