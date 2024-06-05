623d2b0f2c7b172a74c5f38a3cd869e8c29d5023
Ferenc Cole Thierrin <159455998+CThierrin@users.noreply.github.com>
2024-06-05 10:35:20 -0400

    Update liberbatch.py

--- liberbatch.py
+++ liberbatch.py
@@ -1,12 +1,13 @@
 import os, meichecker, liberupdatev5
+PATH = "your path here"
 
-directory = os.fsencode("C:\\Users\\cole_\\Downloads\\liberUsualis - Copy\\liber")
+directory = os.fsencode(PATH)
 err=""
 
 for mefile in os.listdir(directory):
     filename = os.fsdecode(mefile)
     if filename.endswith("corr.mei"):
-        liberupdate.main(filename)
+        liberupdatev5.main(filename)
         print(filename +" has been updated")
         err= err+ (meichecker.main(filename[:-4]+"NEW2.mei")+"\n")
 #        print(err)
