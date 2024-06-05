4a7167bb1b0dcb990acd2c3f5f799eb33e646aac
Ferenc Cole Thierrin <159455998+CThierrin@users.noreply.github.com>
2024-06-05 15:01:21 -0400

    Update code to be more in line with pylint

--- liberbatch.py
+++ liberbatch.py
@@ -1,4 +1,6 @@
-import os, meichecker, liberupdatev5
+import os
+import meichecker
+import liberupdatev5
 PATH = "your path here"
 
 directory = os.fsencode(PATH)
@@ -6,10 +8,10 @@ err=""
 
 for mefile in os.listdir(directory):
     filename = os.fsdecode(mefile)
-    if filename.endswith("corr.mei"):
-        liberupdatev5.main(filename)
-        print(filename +" has been updated")
-        err= err+ (meichecker.main(filename[:-4]+"NEW2.mei")+"\n")
-#        print(err)
-        print(filename +" has been checked")
+   
+    liberupdatev5.main(filename)
+    print(filename +" has been updated")
+    err += f"{meichecker.main(filename[:-4]+"NEW2.mei} \n"
+    print(filename +" has been checked")
+
 print(err)
